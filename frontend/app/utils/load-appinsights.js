7d3c3801f4222d920c44d5d0394fd1c64f547d9d
James Nimmo <james@nimmo.net.nz>
2024-04-05 01:31:55 +0000

    Test application insights

---
+++ frontend/app/utils/load-appinsights.js
@@ -0,0 +1,17 @@
+if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
+  let appInsights = require("applicationinsights");
+  appInsights
+    .setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
+    .setAutoCollectConsole(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectHeartbeat(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectRequests(true)
+    .setAutoDependencyCorrelation(true)
+    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+    .setSendLiveMetrics(true)
+    .setUseDiskRetryCaching(true);
+  appInsights.defaultClient.setAutoPopulateAzureProperties(true);
+  appInsights.start();
+}
