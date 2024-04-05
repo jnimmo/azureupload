ed4b79220a1966a8f3363d09908dced469df5066
James Nimmo <james@nimmo.net.nz>
2024-04-05 01:01:06 +0000

    Add support for Azure Managed Identity

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ NEXT_PUBLIC_STORAGE_ACCOUNT=<storage account name>
 SECRET_ACCESS_KEY=<storage account key>
 ```
 
+If the SECRET_ACCESS_KEY is not defined, the managed identity configured in Azure Static Web Apps will be used instead. This will only need rights to create containers and write files to the given storage account.
+
 ## Install dependencies
 
 ```
--- frontend/app/data/getAuthenticatedBlobClient.tsx
+++ frontend/app/data/getAuthenticatedBlobClient.tsx
@@ -7,41 +7,52 @@ import {
   experimental_taintUniqueValue,
 } from "react";
 import "server-only";
+import { DefaultAzureCredential } from "@azure/identity";
 
 export async function getAuthenticatedBlobStorageClient(): Promise<BlobServiceClient | null> {
   const storageAccountName = process.env.NEXT_PUBLIC_STORAGE_ACCOUNT;
-  const secretAccountKey = process.env.SECRET_ACCESS_KEY;
-
-  // Corrected error message
-  if (!storageAccountName || !secretAccountKey) {
+  if (!storageAccountName) {
     console.error("Error: Missing storage account name or secret access key.");
     return null;
   }
 
-  experimental_taintUniqueValue(
-    "Do not pass secrets to the client",
-    globalThis,
-    secretAccountKey
-  );
-
   try {
-    const sharedKeyCredential = new StorageSharedKeyCredential(
-      storageAccountName,
-      secretAccountKey
-    );
-    const blobServiceClient = new BlobServiceClient(
-      `https://${storageAccountName}.blob.core.windows.net`,
-      sharedKeyCredential
-    );
+    if (process.env.SECRET_ACCESS_KEY) {
+      // Use SAS key
+      experimental_taintUniqueValue(
+        "Do not pass secrets to the client",
+        globalThis,
+        process.env.SECRET_ACCESS_KEY
+      );
+      const credential = new StorageSharedKeyCredential(
+        storageAccountName,
+        process.env.SECRET_ACCESS_KEY
+      );
 
-    experimental_taintObjectReference(
-      "Do not pass the blob storage client to the client",
-      blobServiceClient
-    );
-
-    return blobServiceClient; // Ensure that the ContainerClient object is returned here
+      const blobServiceClient = new BlobServiceClient(
+        `https://${storageAccountName}.blob.core.windows.net`,
+        credential
+      );
+      experimental_taintObjectReference(
+        "Do not pass secrets to the client",
+        blobServiceClient
+      );
+      return blobServiceClient;
+    } else {
+      // Use the managed identity from Azure static web apps
+      const credential = new DefaultAzureCredential();
+      const blobServiceClient = new BlobServiceClient(
+        `https://${storageAccountName}.blob.core.windows.net`,
+        credential
+      );
+      experimental_taintObjectReference(
+        "Do not pass secrets to the client",
+        blobServiceClient
+      );
+      return blobServiceClient;
+    }
   } catch (error) {
-    console.error("Error creating container: ", error);
+    console.error("Error creating blob service client: ", error);
     return null;
   }
 }
